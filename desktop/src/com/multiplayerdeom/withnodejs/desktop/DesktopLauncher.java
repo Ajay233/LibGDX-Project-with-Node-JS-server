7ba86902810e0601bf6649cf86917050679b53ee
Ajay <ajaymungur@hotmail.com>
2019-01-23 07:55:00 +0000

    Created Node JS server with multi client connection

---
+++ desktop/src/com/multiplayerdeom/withnodejs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.multiplayerdeom.withnodejs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.multiplayerdeom.withnodejs.MultiplayerDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MultiplayerDemo(), config);
+	}
+}
